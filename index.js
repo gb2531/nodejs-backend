cc76ca26c6afd0825228ce32df3c0f50dc7cfe0d
suryanarayananhimpact <115137473+suryanarayananhimpact@users.noreply.github.com>
2023-04-08 16:00:51 +0530

    mongodb data base created and connected with nodejs

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+//Database connection
+mongoose.connect("mongodb+srv://devuser:devuser@cluster0.l26prkt.mongodb.net/?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true }).then((res) => {
+  console.log('connection is successful');
+  app.listen(3000);
+}).catch((err) => {
+  console.log("There is an error")
+})
+
+
